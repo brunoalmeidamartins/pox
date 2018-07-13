a78b00f62a6cc893e079a188907aedd127cf7839
BrunoMartins <sgtbrunoalmeida@gmail.com>
2018-07-13 15:56:24 -0300

    13_07_2018_15:56

--- ext/multicast3.py
+++ ext/multicast3.py
@@ -1,21 +1,17 @@
 from pox.core import core
 import pox
 log = core.getLogger()
-
 from pox.lib.packet.ethernet import ethernet, ETHER_BROADCAST
 from pox.lib.packet.ipv4 import ipv4
 from pox.lib.packet.arp import arp
 from pox.lib.addresses import IPAddr, EthAddr
 from pox.lib.util import str_to_bool, dpid_to_str
 from pox.lib.recoco import Timer
-
 import pox.openflow.libopenflow_01 as of
-
 from pox.lib.revent import *
-
 import time
-
 import os
+import json
 
 class multicast (EventMixin):
 #class multicast (object):
@@ -47,6 +43,7 @@ class multicast (EventMixin):
       # This timer handles expiring stuff
       #self._expire_timer = Timer(5, self._handle_expiration, recurring=True)
 
+
       core.listen_to_dependencies(self)
 
 
@@ -64,7 +61,7 @@ class multicast (EventMixin):
         dpid = event.dpid
         inport = event.port
         packet = event.parsed
-        log.info('Pacote de '+ str(self.dpid_to_mac(dpid)))
+        #log.info('Pacote de '+ str(self.dpid_to_mac(dpid)))
 
         net = packet.next # Camada de rede
         transp = packet.next.next # Camada de transporte
@@ -76,29 +73,52 @@ class multicast (EventMixin):
             #.info("Pacote LLDPTYPE")
             #return
         if isinstance(net, ipv4):
-            log.info("Pacote IPv4")
-            log.info("Evento gerado na porta ="+str(inport))
+            #log.info("Pacote IPv4")
+            #log.info("Evento gerado na porta ="+str(inport))
             if (inport == 1 or inport == 2):
                 if(net.protocol != 17):
                     '''
                     Primeiro evento gerado instala a regra para todos os hosts
                     '''
                     #Chama script de execucao dos links principais
+                    log.info("Regras Iniciais Instaladas")
                     os.system('python pox/ext/RegrasIniciais.py')
                 else:
                     if (str(transp.dstport) == '1234'): #Eventos foram gerados por hosts conectados ao swtich mas nao sao udp
-                        if str(self.dpid_to_mac(dpid)) == '00:00:00:00:00:03':
+                        if str(self.dpid_to_mac(dpid)) == '00:00:00:00:00:03': # Switch onde esta os servidores S3
                             msg = of.ofp_flow_mod()
                             #msg.match.dl_src = EthAddr('00:00:00:00:01:01')
                             #msg.match.dl_dst = EthAddr('00:00:00:00:03:11')
-                            msg.priority = 50001
+                            msg.priority = 50001 #seta uma prioridade maior para regra
                             msg.actions.append(of.ofp_action_output(port = 17)) #Nao tem ninguem conectado a essa porta
                             event.connection.send(msg)
                             log.info("Regra de Drop Adicionada!!") # Supoe que drop o pacote
-                #macSrc = net.
-                #log.info("Gerado por:"+str(packet.src))
-                #event.connection.send(self.defineSaidaHost(str(packet.src),inport))
+                            '''
+                            Adiciona o servidor de envio no arquivo IPServidoresMulticast.json
+                            '''
+                            dado = dict(str(net.dstip) = str(transp.port))# IP Servidor e porta
+                            try:
+                                arquivo_json = open("IPServidoresMulticast.json","w")
+                                arquivo_json.write(dado)
+                                arquivo_json.close()
 
+                            except Exception as erro:
+                                log.info("Erro ao abrir arquibo"+erro)
+
+
+                        elif str(self.dpid_to_mac(dpid)) == '00:00:00:00:00:01': # Swithc s1
+                            '''
+                            Fazer as regras aqui!!
+                            '''
+                        elif str(self.dpid_to_mac(dpid)) == '00:00:00:00:00:02': #Switch s2
+                            '''
+                            Fazer as regras aqui!!
+                            '''
+                        elif str(self.dpid_to_mac(dpid)) == '00:00:00:00:00:04': #Swtich s3
+                            '''
+                            Fazer as regras aqui!!
+                            '''
+            '''
             if str(self.dpid_to_mac(dpid)) == '00:00:00:00:00:01':
                 #(inport == 1)
                 msg = of.ofp_flow_mod()
@@ -124,7 +144,7 @@ class multicast (EventMixin):
                 #msg2.actions.append(of.ofp_action_output(port = 1))
                 event.connection.send(self.defineSaidaHost('00:00:00:00:03:11',1))
                 #log.info(msg)
-
+            '''
         elif isinstance(net,arp):
             log.info("Pacote ARP")
             log.info(net)
